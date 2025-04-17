b42b2e4a95331cfc2f29260d6c2c1e78ca652120
Eduard Krivanek <krivanek1234@gmail.com>
2025-04-17 15:16:19 +0200

    chore: removed zonejs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,13 @@ import {
   withFetch,
   withInterceptors,
 } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideFileRouter(),
     provideHttpClient(
       withFetch(),
